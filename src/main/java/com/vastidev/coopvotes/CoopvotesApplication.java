1018f433d4398f7ecaf9653d175aa49bc1d8f973
VastiDev <vastiane1@gmail.com>
2023-11-07 15:58:38 -0300

    config init

---
+++ src/main/java/com/vastidev/coopvotes/CoopvotesApplication.java
@@ -0,0 +1,13 @@
+package com.vastidev.coopvotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoopvotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoopvotesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vastidev/coopvotes/CoopvotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vastidev.coopvotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoopvotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
