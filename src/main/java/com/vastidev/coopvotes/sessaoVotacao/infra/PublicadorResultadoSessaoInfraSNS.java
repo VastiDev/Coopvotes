f4052ce7ada58bfa7f9f41823ed778b381da75d8
VastiDev <vastiane1@gmail.com>
2023-11-18 17:24:02 -0300

    config aws sns e implementa publisher

---
+++ src/main/java/com/vastidev/coopvotes/sessaoVotacao/infra/PublicadorResultadoSessaoInfraSNS.java
@@ -0,0 +1,25 @@
+package com.vastidev.coopvotes.sessaoVotacao.infra;
+
+import com.vastidev.coopvotes.config.AwsConfigProperties;
+import com.vastidev.coopvotes.sessaoVotacao.application.api.ResultadoSessaoResponse;
+import com.vastidev.coopvotes.sessaoVotacao.domain.PublicadorResultadoSessao;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.cloud.aws.messaging.core.NotificationMessagingTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Log4j2
+public class PublicadorResultadoSessaoInfraSNS implements PublicadorResultadoSessao {
+    private final NotificationMessagingTemplate publicadorResultadosSNS;
+    private final AwsConfigProperties awsSnsProperties;
+
+    @Override
+    public void publica(ResultadoSessaoResponse resultadoSessaoResponse) {
+        log.debug("[start] PublicadorResultadoSessaoInfraSNS - publica");
+        publicadorResultadosSNS.sendNotification(awsSnsProperties.getResultadoSessaoTopic(), resultadoSessaoResponse, resultadoSessaoResponse.getIdPauta().toString());
+        log.debug("[finish] PublicadorResultadoSessaoInfraSNS - publica");
+
+    }
+}
